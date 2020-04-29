f5d7c19ebc360943d163055a6e1599011d75ebe0
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2020-04-29 15:50:15 +0900

    implemented mining function

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -3,15 +3,18 @@ use crate::now;
 use crate::transaction::Transaction;
 
 const MINING_DIFFICULTY: usize = 3;
+const MINING_SENDER: &str = "THE BLOCKCHAIN";
+const MINING_REWARD: f32 = 1.0;
 
 #[derive(Debug, Clone)]
 pub struct Blockchain {
     transaction_pool: Vec<Transaction>,
-    chain: Vec<Block>
+    chain: Vec<Block>,
+    blockchain_address: String,
 }
 
 impl Blockchain {
-    pub fn new_blockchain() -> Self {
+    pub fn new_blockchain(blockchain_address: String) -> Self {
         let b = Block{
             nonce: 0,
             previous_hash: "genesis block".to_string(),
@@ -21,6 +24,7 @@ impl Blockchain {
         let mut bc = Blockchain {
             transaction_pool: vec![],
             chain: vec![],
+            blockchain_address: blockchain_address,
         };
         bc.create_block(0, b.hash());
         return bc
@@ -33,8 +37,8 @@ impl Blockchain {
     pub fn last_block(&self) -> &Block {
         &self.chain[self.chain.len()-1]
     }
-    pub fn add_transaction(&mut self, sender: String, recipient: String, value: f32) {
-        let t = Transaction::new_transaction(sender, recipient, value);
+    pub fn add_transaction(&mut self, sender: String, recipient: &String, value: f32) {
+        let t = Transaction::new_transaction(sender, recipient.to_string(), value);
         self.transaction_pool.push(t)
     }
     fn valid_proof(&self, nonce: u32, previous_hash: &String, transactions: &Vec<Transaction>, difficulty: usize) -> bool {
@@ -56,5 +60,12 @@ impl Blockchain {
             nonce += 1;
         }
         nonce
+    }
+    pub fn mining(&mut self) -> bool {
+        self.add_transaction(MINING_SENDER.to_string(), &self.blockchain_address.to_string(), MINING_REWARD);
+        let nonce = self.clone().proof_of_work();
+        let previous_hash = self.last_block().hash();
+        self.create_block(nonce, previous_hash);
+        true
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 use minichain::blockchain::Blockchain;
 
 fn main() {
-    let mut bc = Blockchain::new_blockchain();
+    let my_blockchain_address: String = "my_blockchain_address".to_string();
+    let mut bc = Blockchain::new_blockchain(my_blockchain_address);
 
-    bc.add_transaction("A".to_string(), "B".to_string(), 1.0);
-    let mut previous_hash = bc.last_block().hash();
-    let mut nonce = bc.clone().proof_of_work();
-    bc.create_block(nonce, previous_hash);
+    bc.add_transaction("A".to_string(), &"B".to_string(), 1.0);
+ //   let mut previous_hash = bc.last_block().hash();
+ //   let mut nonce = bc.clone().proof_of_work();
+ //   bc.create_block(nonce, previous_hash);
+    bc.mining();
 
-    bc.add_transaction("C".to_string(), "D".to_string(), 2.0);
-    bc.add_transaction("X".to_string(), "Y".to_string(), 3.0);
-    previous_hash = bc.last_block().hash();
-    nonce = bc.clone().proof_of_work();
-    bc.create_block(nonce, previous_hash);
+    bc.add_transaction("C".to_string(), &"D".to_string(), 2.0);
+    bc.add_transaction("X".to_string(), &"Y".to_string(), 3.0);
+//    previous_hash = bc.last_block().hash();
+//    nonce = bc.clone().proof_of_work();
+//    bc.create_block(nonce, previous_hash);
+    bc.mining();
 
     println!("{:#?}", bc);
 }
