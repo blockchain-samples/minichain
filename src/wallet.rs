8c3150cbd2a1b623627e5357d2af347bb742e667
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2020-05-04 08:30:24 +0900

    minor fix

--- src/wallet.rs
+++ src/wallet.rs
@@ -24,18 +24,18 @@ impl Wallet {
         let h2 = sha256::Hash::hash(&public_key.serialize());
         // 3. Perform RIPEMD-160 hashing on the result of SHA-256.
         let h3 = ripemd160::Hash::hash(&h2).into_inner();
-        // 4 - Add version byte in front of RIPEMD-160 hash.
+        // 4. Add version byte in front of RIPEMD-160 hash.
         let mut h4 = [0u8; 2].to_vec();
         h4.extend(&h3);
-        // 5 - Perform SHA-256 hash on the extended RIPEMD-160 result.
+        // 5. Perform SHA-256 hash on the extended RIPEMD-160 result.
         let h5 = sha256::Hash::hash(&h4);
-        // 6 - Perform SHA-256 hash on the result of the previous SHA-256 hash.
+        // 6. Perform SHA-256 hash on the result of the previous SHA-256 hash.
         let h6 = sha256::Hash::hash(&h5);
-        // 7 - Take the first 4 bytes of the second SHA-256 hash.
+        // 7. Take the first 4 bytes of the second SHA-256 hash.
         let h7 = &h6[..4];
-        // 8 - Add the 4 checksum bytes from stage 7 at the end of extended RIPEMD-160 hash from stage 4.
+        // 8. Add the 4 checksum bytes from stage 7 at the end of extended RIPEMD-160 hash from stage 4.
         h7.to_vec().extend(&h4);
-        // 9 - Convert the result from a byte string into a base58 string using Base58Check encoding.
+        // 9. Convert the result from a byte string into a base58 string using Base58Check encoding.
         Wallet {
             private_key: secret_key,
             public_key: h3,
