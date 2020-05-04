5a4d99b6c2a480c48a92c6ecd897da1357e822f4
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2020-05-04 20:51:02 +0900

    minor fix

--- src/wallet.rs
+++ src/wallet.rs
@@ -17,7 +17,8 @@ impl Wallet {
         let secp = Secp256k1::new();
         let mut rng = OsRng::new().unwrap();
         // 1. Having a private ECDSA key.
-        // 2. Take the corresponding public key generated with it (33 bytes, 1 byte 0x02 (y-coord is even), and 32 bytes corresponding to X coordinate)
+        // 2. Take the corresponding public key generated with it.
+        // (33 bytes, 1 byte 0x02 (y-coord is even), and 32 bytes corresponding to X coordinate)
         let (secret_key, public_key) = secp.generate_keypair(&mut rng);
         // 3. Perform SHA-256 hashing on the public key
         let h3 = sha256::Hash::hash(&public_key.serialize());
@@ -30,12 +31,15 @@ impl Wallet {
         let h6 = sha256::Hash::hash(&h5);
         // 7. Perform SHA-256 hash on the result of the previous SHA-256 hash
         let h7 = sha256::Hash::hash(&h6);
-        // 8. Take the first 4 bytes of the second SHA-256 hash. This is the address checksum
+        // 8. Take the first 4 bytes of the second SHA-256 hash.
+        // This is the address checksum.
         let h8 = &h7[..4];
-        // 9. Add the 4 checksum bytes from stage 7 at the end of extended RIPEMD-160 hash from stage 4. This is the 25-byte binary Bitcoin Address.
+        // 9. Add the 4 checksum bytes from stage 7 at the end of extended RIPEMD-160 hash from stage 4.
+        // This is the 25-byte binary Bitcoin Address.
         let mut h9 = h5.clone();
         h9.extend(h8);
-        // 10. Convert the result from a byte string into a base58 string using Base58Check encoding. This is the most commonly used Bitcoin Address format
+        // 10. Convert the result from a byte string into a base58 string using Base58Check encoding.
+        // This is the most commonly used Bitcoin Address format.
         let h10 = bs58::encode(&h9).into_string();
 
         Wallet {
