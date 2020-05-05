31fe1604fa45cebe0ff824bcbcdffa58fa7ab8db
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2020-05-05 19:16:15 +0900

    beaufity wallet

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,12 +1,10 @@
-extern crate secp256k1;
-//extern crate crypto_hash;
 extern crate bitcoin_hashes;
 extern crate hex;
+extern crate secp256k1;
 
+use bitcoin_hashes::{ripemd160, sha256, Hash};
 use secp256k1::rand::rngs::OsRng;
-use secp256k1::{Secp256k1, PublicKey, SecretKey};
-//use crypto_hash::{hex_digest, Algorithm};
-use bitcoin_hashes::{sha256, Hash, ripemd160};
+use secp256k1::{PublicKey, Secp256k1, SecretKey};
 
 pub struct Wallet {
     pub private_key: SecretKey,
@@ -18,36 +16,46 @@ impl Wallet {
     pub fn new_wallet() -> Self {
         let secp = Secp256k1::new();
         let mut rng = OsRng::new().unwrap();
+
         // 1. Having a private ECDSA key.
         // 2. Take the corresponding public key generated with it.
-        // (33 bytes, 1 byte 0x02 (y-coord is even), and 32 bytes corresponding to X coordinate)
         let (secret_key, public_key) = secp.generate_keypair(&mut rng);
-        // 3. Perform SHA-256 hashing on the public key
-        let h3 = sha256::Hash::hash(&public_key.serialize());
-        // 4. Perform RIPEMD-160 hashing on the result of SHA-256
-        let h4 = ripemd160::Hash::hash(&h3).into_inner();
-        // 5. Add version byte in front of RIPEMD-160 hash (0x00 for Main Network)
-        let mut h5 = [0u8; 1].to_vec();
-        h5.extend(&h4);
-        // 6. Perform SHA-256 hash on the extended RIPEMD-160 result
-        let h6 = sha256::Hash::hash(&h5);
-        // 7. Perform SHA-256 hash on the result of the previous SHA-256 hash
-        let h7 = sha256::Hash::hash(&h6);
+
+        // 3. Perform SHA-256 hashing on the public key.
+        let sha256_public_key = sha256::Hash::hash(&public_key.serialize());
+
+        // 4. Perform RIPEMD-160 hashing on the result of SHA-256.
+        let ripmd160_sha256_public_key = ripemd160::Hash::hash(&sha256_public_key).into_inner();
+
+        // 5. Add version byte in front of RIPEMD-160 hash (0x00 for Main Network).
+        let mut extended_ripmd160_sha256_public_key = [0u8; 1].to_vec();
+        extended_ripmd160_sha256_public_key.extend(&ripmd160_sha256_public_key);
+
+        // 6. Perform SHA-256 hash on the extended RIPEMD-160 result.
+        let sha256_extended_ripmd160_sha256_public_key =
+            sha256::Hash::hash(&extended_ripmd160_sha256_public_key);
+
+        // 7. Perform SHA-256 hash on the result of the previous SHA-256 hash.
+        let double_sha256_extended_ripmd160_sha256_public_key =
+            sha256::Hash::hash(&sha256_extended_ripmd160_sha256_public_key);
+
         // 8. Take the first 4 bytes of the second SHA-256 hash.
         // This is the address checksum.
-        let h8 = &h7[..4];
+        let checksum = &double_sha256_extended_ripmd160_sha256_public_key[..4];
+
         // 9. Add the 4 checksum bytes from stage 7 at the end of extended RIPEMD-160 hash from stage 4.
         // This is the 25-byte binary Bitcoin Address.
-        let mut h9 = h5.clone();
-        h9.extend(h8);
+        let mut binary_bitcoin_address = extended_ripmd160_sha256_public_key.clone();
+        binary_bitcoin_address.extend(checksum);
+
         // 10. Convert the result from a byte string into a base58 string using Base58Check encoding.
         // This is the most commonly used Bitcoin Address format.
-        let h10 = bs58::encode(&h9).into_string();
+        let bitcoin_address = bs58::encode(&binary_bitcoin_address).into_string();
 
         Wallet {
             private_key: secret_key,
             public_key: public_key,
-            blockchain_address: h10,
+            blockchain_address: bitcoin_address,
         }
     }
 }
